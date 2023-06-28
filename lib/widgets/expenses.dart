632b6eb4f980bfefdf90f84ce0986328b70ca4e9
unknown <petreistvan94@gmail.com>
2023-06-28 15:40:57 +0200

    chart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
@@ -91,7 +92,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The cart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
